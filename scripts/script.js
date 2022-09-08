2deae26a20d4a211000591a3cdd266ff018615a5
viktor-drok <vityadrok@gmail.com>
2022-09-08 13:51:06 +0300

    show code in html

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,38 @@ class Circle {
 const circle = new Circle({
     radius: '20'
 });
+
+document.querySelector('span').style.color = "green";
+
+document.querySelector('span').innerText = `class Circle {
+    static type = 'circle';
+
+    constructor (options) {
+        this.radius = options.radius;
+    }
+
+    get circleRadius() {
+        console.log(this.radius);
+    }
+
+    get circleDiameter() {
+        console.log(this.radius * 2);
+    }
+
+    set circleRadius(newRadius) {
+        this.radius = newRadius;
+    }
+
+    circleArea() {
+        return (Math.PI * (this.radius ** 2)).toFixed(2);
+    }
+
+    circuit() {
+        return (2 * Math.PI * this.radius).toFixed(2);
+    }
+
+}
+
+const circle = new Circle({
+    radius: '20'
+});`;
